435a6b7ef40b4839a89ce8db3eb00e9b5b1f16b0
martinmalinda <malindacz@gmail.com>
2021-10-03 09:30:53 +0200

    add a feedback link

--- docs/.vitepress/theme/HeroLayout.vue
+++ docs/.vitepress/theme/HeroLayout.vue
@@ -1,16 +1,40 @@
 <script setup>
 import DefaultTheme from 'vitepress/theme';
-import { useData, useRoute } from 'vitepress'
+import { useData, useRoute, useRouter} from 'vitepress';
+import { computed } from 'vue';
+
+const data = useData();
+const language = computed(() => data.lang.value);
+const router = useRouter();
+const pageData = computed(() => router.route.data);
+const feedbackLink = computed(() => {
+  if (language.value.includes('cs')) {
+    return `https://airtable.com/shr6t223A3G1XNqfc?prefill_Stránka=${pageData.value.title}`;
+  }
+
+  return `https://airtable.com/shrLoqNku6HTrM4I1?prefill_Page=${pageData.value.title}`;
+});
 
 const { Layout } = DefaultTheme
-
-const foo = useData();
-console.log({ foo });
 </script>
 
 <template>
   <Layout>
-    <template #page-top>
+    <template #page-bottom>
+      <small>
+      <template v-if="language.includes('cs')">
+        Něco tu chybí?
+        <a target="_blank" rel="noreferrer noopener" :href="feedbackLink">
+          Dejte nám vědět co bychom tu mohli zlepšit
+        </a>
+      </template>
+      <template v-else-if="language.includes('en')">
+        Something is missing?
+        <a target="_blank" rel="noreferrer noopener" :href="feedbackLink">
+          Let us know so we can make it better
+        </a>
+      </template>
+      </small>
     </template>
   </Layout>
 </template>
