47b7f4dc0d1a5df076ccc8365875e8e22b9dab9f
martinmalinda <malindacz@gmail.com>
2022-07-04 21:26:32 +0200

    add logo

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,14 +1,14 @@
 const { default: pages } = require("../../scripts/pages.mjs");
 
 module.exports = {
-  logo: 'https://herohero.co/public/icons/safari-pinned-tab.svg',
+  logo: 'https://media-exp1.licdn.com/dms/image/C4E0BAQEPSBEOddfbHw/company-logo_200_200/0/1656513521414?e=1665014400&v=beta&t=Ea5GQmC2SOlIdVTfoO4eDdl6V38x1OZxbZ6TYT50OBc',
   title: 'Martin\'s automations',
   description: 'Frequently asked questions about the Herohero platform - Content, Payments, Subscriptions and so on.',
   head: [
   ],
 
   themeConfig: {
-    logo: 'https://herohero.co/public/icons/safari-pinned-tab.svg',
+    logo: 'https://media-exp1.licdn.com/dms/image/C4E0BAQEPSBEOddfbHw/company-logo_200_200/0/1656513521414?e=1665014400&v=beta&t=Ea5GQmC2SOlIdVTfoO4eDdl6V38x1OZxbZ6TYT50OBc',
     nav: [
       { text: 'Back to the website', link: 'https:/automation.martinmalinda.cz' }
     ],
