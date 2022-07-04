6356c8dc141fc91adf957d4354ce49a0bcbb6271
martinmalinda <malindacz@gmail.com>
2022-07-04 22:49:32 +0200

    fix link

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -10,7 +10,7 @@ module.exports = {
   themeConfig: {
     logo: 'https://media-exp1.licdn.com/dms/image/C4E0BAQEPSBEOddfbHw/company-logo_200_200/0/1656513521414?e=1665014400&v=beta&t=Ea5GQmC2SOlIdVTfoO4eDdl6V38x1OZxbZ6TYT50OBc',
     nav: [
-      { text: 'Back to the website', link: 'https:/automation.martinmalinda.cz' }
+      { text: 'Back to the website', link: 'https://automation.martinmalinda.cz' }
     ],
 
     sidebar: pages.map(page => {
